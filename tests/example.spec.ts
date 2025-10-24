0a0302588ad50981b11323fc4127229668949d06
tobiasvdorp <tobias.vandorp@student.hu.nl>
2025-10-24 14:11:46 +0200

    init playwright for e2e testing

---
+++ tests/example.spec.ts
@@ -0,0 +1,20 @@
+import { test, expect } from "@playwright/test";
+
+test("has title", async ({ page }) => {
+  await page.goto("localhost:3000");
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/Tobias van Dorp/);
+});
+
+test("get started link", async ({ page }) => {
+  await page.goto("localhost:3000");
+
+  // Click the get started link.
+  await page.getByRole("link", { name: "View projects" }).click();
+
+  // Expects page to have a heading with the name of Installation.
+  // await expect(
+  //   page.getByRole("heading", { name: "Installation" })
+  // ).toBeVisible();
+});
